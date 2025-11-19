4047933dc62d6fa89e2eef9a9ba2fcb684fd924c
Dale Peligro <ddparmstrong2021@gmail.com>
2025-11-19 15:49:43 -0800

    added page switching functionality

--- src/index.js
+++ src/index.js
@@ -1,2 +1,36 @@
 import "./styles.css";
 
+const studentResourcesButton = document.getElementById('student-resources-button');
+const studentResourcesNav = document.getElementById('student-resources-nav');
+studentResourcesButton.addEventListener('click', () => {
+  console.log('button clicked');
+  if (studentResourcesNav.classList.contains('hidden')) {
+    studentResourcesNav.classList.remove('hidden');
+    studentResourcesButton.innerHTML = `
+      Student Resources <span class="nav-open">^</span>`;
+  } else {
+    studentResourcesNav.classList.add('hidden');
+    studentResourcesButton.innerHTML = `Student Resources <span class="nav-closed">⌄</span> `;
+  }
+
+});
+
+const pages = document.querySelectorAll('.page');
+const navButtons = document.querySelectorAll('.nvb');
+navButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const target = button.dataset.page;
+
+    // if button has no page do nothing
+    if (!target) return;
+
+    // hide all pages
+    pages.forEach(p => p.classList.add('hidden'));
+
+    // show the slected one
+    document.getElementById(target).classList.remove('hidden');
+
+    navButtons.forEach(b => b.classList.remove('selected'));
+    button.classList.add('selected');
+  });
+});
