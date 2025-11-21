5ff08e36ea5cbbe33a2ac10f73a83388c8806953
Dale Peligro <ddparmstrong2021@gmail.com>
2025-11-20 19:44:40 -0800

    tweaked research lab buttons functionality

--- src/index.js
+++ src/index.js
@@ -67,22 +67,42 @@ greenStudentResourcesButton.addEventListener('click', () => {
   toggleStudentResourcesTab();
 });
 
-const researchLabButtons = document.querySelectorAll('.research-lab-button');
+const researchLabButtons = document.querySelectorAll('.lab-button');
 const researchLabSections = document.querySelectorAll('.labsection');
 researchLabButtons.forEach(button => {
   button.addEventListener('click', () => {
     const target = button.dataset.labsection;
-
     if (!target) return;
 
-    researchLabSections.forEach(s => s.classList.add('hidden'));
-    document.getElementById(target).classList.remove('hidden');
+    const section = document.getElementById(target);
+
+    const wasSelected = button.classList.contains('lab-button-selected');
 
     researchLabButtons.forEach(b => {
       b.classList.remove('lab-button-selected');
       b.classList.add('research-lab-button');
     });
 
+    researchLabSections.forEach(s => {
+      s.classList.add('max-h-0');  
+      s.classList.remove('pt-3');
+      s.classList.remove('pb-5');
+      s.classList.remove('max-h-[500px]') 
+    });
+
+    if (wasSelected) {
+      section.classList.add('max-h-0');  
+      section.classList.remove('pt-3');
+      section.classList.remove('pb-5');
+      section.classList.remove('max-h-[500px]');    
+      button.classList.add('research-lab-button'); 
+      return;
+    }
+
+    section.classList.remove('max-h-0');
+    section.classList.add('pt-3');
+    section.classList.add('pb-5');
+    section.classList.add('max-h-[500px]')
     button.classList.add('lab-button-selected');
     button.classList.remove('research-lab-button');
   });
